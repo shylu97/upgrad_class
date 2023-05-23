e3954f5a3819c409650251b652b8261e17462842
patil11 <rushal.patil@here.com>
2023-05-23 21:42:29 +0530

    new file addeed

---
+++ RushalPatil/Array2DExample.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Array2DExample {
+
+
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[3][5];
+
+        Scanner sc = new Scanner(System.in) ;
+
+        System.out.println("Please enter numbers");
+        System.out.println(matrix.length);
+
+        for(int i = 0 ; i< matrix.length ; i ++){
+
+            for (int j = 0 ; j< matrix[i].length ; j++){
+
+
+            }
+
+        }
+
+
+    }
+
+
+
+}
